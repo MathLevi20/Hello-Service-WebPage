ac718f5a59d24c9f6849d5bf8ea300cf1962aae5
Matheus Levi <matheuslevi545@gmail.com>
2022-10-03 16:25:36 -0300

    feat: Mode Sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './index.css'
-import iphone from './assets/mediamodifier_image.png'
+import iphone from './assets/iphone_mockup.png'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -23,12 +23,11 @@ function App() {
             <span className="ml-3 text-xl">Hello Service</span>
           </a>
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-            <a className="mr-5 hover:text-white">First Link</a>
-            <a className="mr-5 hover:text-white">Second Link</a>
-            <a className="mr-5 hover:text-white">Third Link</a>
-            <a className="mr-5 hover:text-white">Fourth Link</a>
+            <a className="mr-5 hover:text-white">Aplicativo</a>
+            <a className="mr-5 hover:text-white">Opinião</a>
+
           </nav>
-          <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
+          <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Login
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
@@ -66,7 +65,7 @@ function App() {
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img className="object-cover object-center rounded transform transition-all hover:scale-125" alt="hero" src={iphone} />
+            <img className="object-cover object-center rounded transform transition-all animate-fade-in-down" alt="hero" src={iphone} />
           </div>
         </div>
       </section>
@@ -107,8 +106,34 @@ function App() {
           </div>
         </div>
       </section>
+      <section className=" text-black body-font bg-[#F8ED31]">
+        <div className="container  mx-auto flex flex-wrap">
+          <div className="lg:w-3/5 w-full mb-10 lg:mb-0 overflow-hidden">
+            <img alt="feature" className="object-cover object-center h-full w-full" src="https://www.casamagalhaes.com.br/blog/wp-content/uploads/2016/08/transforme-o-seu-garcom-em-um-vendedor-de-sucesso.png" />
+          </div>
+          <div className="lg:w-1/3 md:w-1/2 md:pr-16 lg:pr-0 pr-0 text-left self-center px-10">
+            <h1 className="title-font font-medium text-3xl text-gray-900 ">Por que contratar um garçom?</h1>
+            <p className="leading-relaxed mt-4">A necessidade de novos funcionários pode afetar sua equipe existente e seus resultados. Uma ótima contratação de garçom pode ajudar sua empresa:
+            </p>
+            <ul className='list-disc px-4 py-2'>
+              <li>Receber os pedidos dos clientes e transmiti-los à equipe da cozinha</li>
+              <li>Sugerir itens de menu com base nos desejos dos clientes</li>
+              <li>Oferecer sugestões aos clientes sobre opções de comida e vinho</li>
+            </ul>
+          </div>
 
-      <footer className="text-gray-600 body-font">
+        </div>
+      </section>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Como é o mercado de trabalho para Garçom?</h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Este profissional está presente em bares, restaurantes, lanchonetes, buffets para eventos e diversos outros estabelecimentos de entretenimento que trabalham com a oferta de alimentos e bebidas para consumo no local. O Garçom precisa ser uma pessoa muito atenciosa, cortês, paciente, prestativa, ágil, organizada, flexível, com boa comunicação e desenvoltura e que goste de atuar com o atendimento direto ao público.</p>
+          </div>
+
+        </div>
+      </section>
+      <footer className="text-gray-600 body-font bg-slate-100">
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
             <svg width="50" height="50" viewBox="0 0 98 98" fill="none" xmlns="http://www.w3.org/2000/svg">
